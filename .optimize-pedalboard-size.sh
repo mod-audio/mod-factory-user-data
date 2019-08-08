4610ba8e13ea0eb1f1145acb2acb377041f5410e
falkTX <falktx@gmail.com>
2019-08-08 16:02:58 +0100

    Add script to optimize pedalboard size
    Signed-off-by: falkTX <falktx@gmail.com>

---
+++ .optimize-pedalboard-size.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)
+
+rm -f {modduo,modduox}/.pedalboards/*.pedalboard/screenshot.png
+
+for file in $(find . -name "*.png"); do
+  pngcrush ${file} ${file}.new
+  mv ${file}.new ${file}
+done
