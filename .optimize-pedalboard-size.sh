6584cbf84e98e81b7ef7173ef5ce038b9cff37bf
falkTX <falktx@falktx.com>
2023-02-13 16:36:39 +0100

    Add dwarf VERB pedalboards and bank
    Signed-off-by: falkTX <falktx@falktx.com>

--- .optimize-pedalboard-size.sh
+++ .optimize-pedalboard-size.sh
@@ -4,9 +4,11 @@ set -e
 
 cd $(dirname $0)
 
-rm -f {modduo,modduox,moddwarf}/.pedalboards/*.pedalboard/screenshot.png
+# {modduo,modduox,moddwarf}
 
-for file in $(find . -name "*.png"); do
+rm -f moddwarf/.pedalboards/*.pedalboard/screenshot.png
+
+for file in $(find moddwarf -name "*.png"); do
   pngcrush ${file} ${file}.new
   mv ${file}.new ${file}
 done
